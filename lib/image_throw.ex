3f20b5946f727688ce2850b95fbb9f4f4ce7c4be
Jim Knight <jimknight@lavatech.com>
2020-06-05 10:40:56 -0400

    Fresh

---
+++ lib/image_throw.ex
@@ -0,0 +1,18 @@
+defmodule ImageThrow do
+  @moduledoc """
+  Documentation for `ImageThrow`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ImageThrow.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/image_throw_test.exs
@@ -0,0 +1,8 @@
+defmodule ImageThrowTest do
+  use ExUnit.Case
+  doctest ImageThrow
+
+  test "greets the world" do
+    assert ImageThrow.hello() == :world
+  end
+end
